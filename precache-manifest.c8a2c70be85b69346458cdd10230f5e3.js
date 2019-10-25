e934a538e8682092aa334f6aace13fe4fec6bfde
Glievoyss <glievoy.s@gmail.com>
2019-10-25 17:31:39 +0300

    Updates

---
+++ precache-manifest.c8a2c70be85b69346458cdd10230f5e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f2d320771e15df0b3363a9c7fd8fbd",
+    "url": "/goit-react-hw-05-budget-planner/./index.html"
+  },
+  {
+    "revision": "58647ffe0aa1b6401f57",
+    "url": "/goit-react-hw-05-budget-planner/./static/css/main.6b053a12.chunk.css"
+  },
+  {
+    "revision": "7fcc1cb828f82fe7c60e",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/2.d7c59537.chunk.js"
+  },
+  {
+    "revision": "58647ffe0aa1b6401f57",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/main.5a229354.chunk.js"
+  },
+  {
+    "revision": "e4dae49ae63a4e8bfd0e",
+    "url": "/goit-react-hw-05-budget-planner/./static/js/runtime-main.2c1bd70e.js"
+  }
+]);
